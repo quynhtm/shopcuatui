9aa8c08e76d4629a63cf288f4f87ed36d3ae3f7c
nguyenduypt86@gmail.com <nguyenduypt86@gmail.com>
2017-04-25 15:20:00 +0700

    duy

--- app/views/site/SiteLayouts/Home.blade.php
+++ app/views/site/SiteLayouts/Home.blade.php
@@ -4,39 +4,37 @@
             <div class="content-box-menu">
                 <ul>
                     <li>
-                        <a href="/c-246/dich-vu.html" title="Dịch vụ">Dịch vụ</a>
+                        <a href="" title="Dịch vụ">Dịch vụ</a>
                         <div class="list-subcat" style="background: #fff ">
                             <ul>
-                                <li><a href="/c-247/nau-co.html" title="Nấu cỗ">Nấu cỗ</a></li>
-                                <li><a href="/c-248/dam-hoi-dam-cuoi.html" title="Đám hỏi - Đám cưới">Đám hỏi - Đám cưới</a></li>
-                                <li><a href="/c-249/dam-hieu.html" title="Đám hiếu">Đám hiếu</a></li>
-                                <li><a href="/c-250/o-sin-giup-viec.html" title="Ô sin - giúp việc">Ô sin - giúp việc</a></li>
-                                <li><a href="/c-251/chup-anh.html" title="Chụp ảnh">Chụp ảnh</a></li>
-                                <li><a href="/c-252/bien-phien-dich.html" title="Biên - phiên dịch">Biên - phiên dịch</a></li>
+                                <li><a href="" title="Nấu cỗ">Nấu cỗ</a></li>
+                                <li><a href="" title="Đám hỏi - Đám cưới">Đám hỏi - Đám cưới</a></li>
+                                <li><a href="" title="Đám hiếu">Đám hiếu</a></li>
+                                <li><a href="" title="Ô sin - giúp việc">Ô sin - giúp việc</a></li>
+                                <li><a href="" title="Chụp ảnh">Chụp ảnh</a></li>
+                                <li><a href="" title="Biên - phiên dịch">Biên - phiên dịch</a></li>
                             </ul>
+                            <img class="bg-absolute" src="{{Config::get('config.WEB_ROOT')}}/assets/frontend/img/bg-cat/dam-xoe.png">
                         </div>
                     </li>
                     <li>
-                        <a href="/c-164/my-pham-lam-dep.html" title="Mỹ phẩm - làm đẹp">Mỹ phẩm - làm đẹp</a>
+                        <a href="" title="Mỹ phẩm - làm đẹp">Mỹ phẩm - làm đẹp</a>
                         <div class="list-subcat" style="background: #fff ">
                             <ul>
-                                <li><a href="/c-165/trang-diem-phu-kien.html" title="Trang điểm - phụ kiện">Trang điểm - phụ kiện</a></li>
-                                <li><a href="/c-166/cham-soc-co-the.html" title="Chăm sóc cơ thể">Chăm sóc cơ thể</a></li>
-                                <li><a href="/c-167/cham-soc-toc.html" title="Chăm sóc tóc">Chăm sóc tóc</a></li>
-                                <li><a href="/c-168/cham-soc-mat.html" title="Chăm sóc mặt">Chăm sóc mặt</a></li>
-                                <li><a href="/c-176/thiet-bi-phu-kien-lam-dep.html" title="Thiết bị, phụ kiện làm đẹp">Thiết bị, phụ kiện làm đẹp</a></li>
-                                <li><a href="/c-183/nuoc-hoa-chat-tao-huong.html" title="Nước hoa,chất tạo hương">Nước hoa,chất tạo hương</a></li>
-                                <li><a href="/c-198/my-pham-cho-nam-gioi.html" title="Mỹ phẩm cho nam giới">Mỹ phẩm cho nam giới</a></li>
-                                <li><a href="/c-199/cham-soc-mong-tay-chan.html" title="Chăm sóc móng, tay, chân">Chăm sóc móng, tay, chân</a></li>
-                                <li><a href="/c-200/cham-soc-da.html" title="Chăm sóc da">Chăm sóc da</a></li>
-                                <li><a href="/c-201/cham-soc-suc-khoe.html" title="Chăm sóc sức khỏe">Chăm sóc sức khỏe</a></li>
+                                <li><a href="" title="Trang điểm - phụ kiện">Trang điểm - phụ kiện</a></li>
+                                <li><a href="" title="Chăm sóc cơ thể">Chăm sóc cơ thể</a></li>
+                                <li><a href="" title="Chăm sóc tóc">Chăm sóc tóc</a></li>
+                                <li><a href="" title="Chăm sóc mặt">Chăm sóc mặt</a></li>
+                                <li><a href="" title="Thiết bị, phụ kiện làm đẹp">Thiết bị, phụ kiện làm đẹp</a></li>
+                                <li><a href="" title="Nước hoa,chất tạo hương">Nước hoa,chất tạo hương</a></li>
+                                <li><a href="" title="Mỹ phẩm cho nam giới">Mỹ phẩm cho nam giới</a></li>
                             </ul>
                             <ul>
-                                <li><a href="/c-202/my-pham-cho-tre-em.html" title="Mỹ phẩm cho trẻ em">Mỹ phẩm cho trẻ em</a></li>
-                                <li><a href="/c-203/my-pham-xach-tay.html" title="Mỹ phẩm xách tay">Mỹ phẩm xách tay</a></li>
-                                <li><a href="/c-204/my-pham-tu-che.html" title="Mỹ phẩm tự chế">Mỹ phẩm tự chế</a></li>
-                                <li><a href="/c-205/spa-massage.html" title="Spa &amp; Massage">Spa &amp; Massage</a></li>
-                                <li><a href="/c-206/dich-vu-trang-diem.html" title="Dịch vụ trang điểm">Dịch vụ trang điểm</a></li>
+                                <li><a href="" title="Mỹ phẩm cho trẻ em">Mỹ phẩm cho trẻ em</a></li>
+                                <li><a href="" title="Mỹ phẩm xách tay">Mỹ phẩm xách tay</a></li>
+                                <li><a href="" title="Mỹ phẩm tự chế">Mỹ phẩm tự chế</a></li>
+                                <li><a href="" title="Spa &amp; Massage">Spa &amp; Massage</a></li>
+                                <li><a href="" title="Dịch vụ trang điểm">Dịch vụ trang điểm</a></li>
                             </ul>
                         </div>
                     </li>
@@ -44,12 +42,6 @@
                         <a href="/c-97/thoi-trang.html" title="Thời trang">Thời trang</a>
                         <div class="list-subcat" style="background: #fff ">
                             <ul>
-                                <li><a href="/c-98/ao-somi-nam.html" title="Áo sơmi nam">Áo sơmi nam</a></li>
-                                <li><a href="/c-99/ao-khoac-vest-nam.html" title="Áo khoác, Vest nam">Áo khoác, Vest nam</a></li>
-                                <li><a href="/c-102/quan-va-ao-phong-nam.html" title="Quần và Áo phông nam">Quần và Áo phông nam</a></li>
-                                <li><a href="/c-103/do-lot-do-boi-nam.html" title="Đồ lót, Đồ bơi nam">Đồ lót, Đồ bơi nam</a></li>
-                                <li><a href="/c-104/do-the-thao-mac-nha-nam.html" title="Đồ thể thao, mặc nhà nam">Đồ thể thao, mặc nhà nam</a></li>
-                                <li><a href="/c-106/dam-vay-nu.html" title="Đầm, váy Nữ">Đầm, váy Nữ</a></li>
                                 <li><a href="/c-107/ao-so-mi-nu.html" title="Áo sơ mi nữ">Áo sơ mi nữ</a></li>
                                 <li><a href="/c-108/ao-khoac-va-vest-nu.html" title="Áo Khoác và Vest Nữ">Áo Khoác và Vest Nữ</a></li>
                                 <li><a href="/c-110/do-lot-do-boi-nu.html" title="Đồ lót, đồ bơi nữ">Đồ lót, đồ bơi nữ</a></li>
@@ -59,12 +51,6 @@
                                 <li><a href="/c-113/quan-chan-vay-nu.html" title="Quần &amp; chân váy nữ">Quần &amp; chân váy nữ</a></li>
                                 <li><a href="/c-115/phu-kien-thoi-trang-nu.html" title="Phụ kiện thời trang Nữ">Phụ kiện thời trang Nữ</a></li>
                                 <li><a href="/c-116/phu-kien-thoi-trang-tre-em.html" title="Phụ kiện thời trang trẻ em">Phụ kiện thời trang trẻ em</a></li>
-                                <li><a href="/c-119/phu-kien-thoi-trang-nam.html" title="Phụ kiện thời trang Nam">Phụ kiện thời trang Nam</a></li>
-                                <li><a href="/c-122/giay-dep-tui-xach-tre-em.html" title="Giày dép, túi xách trẻ em">Giày dép, túi xách trẻ em</a></li>
-                                <li><a href="/c-127/giay-dep-tui-xach-nu.html" title="Giày dép, túi xách Nữ">Giày dép, túi xách Nữ</a></li>
-                                <li><a href="/c-139/giay-dep-tui-sach-nam.html" title="Giày dép, túi sách Nam">Giày dép, túi sách Nam</a></li>
-                                <li><a href="/c-180/thoi-trang-tween-teen.html" title="Thời trang Tween &amp; Teen">Thời trang Tween &amp; Teen</a></li>
-                                <li><a href="/c-181/thoi-trang-tre-em.html" title="Thời trang trẻ em">Thời trang trẻ em</a></li>
                                 <li><a href="/c-182/thoi-trang-painting-handmade.html" title="Thời trang painting - handmade">Thời trang painting - handmade</a></li>
                             </ul>
                         </div>
@@ -73,10 +59,6 @@
                         <a href="/c-90/thuc-pham.html" title="Thực phẩm">Thực phẩm</a>
                         <div class="list-subcat" style="background: #fff ">
                             <ul>
-                                <li><a href="/c-91/thuc-pham-che-bien-san.html" title="Thực phẩm chế biến sẵn">Thực phẩm chế biến sẵn</a></li>
-                                <li><a href="/c-93/thuc-pham-kho.html" title="Thực phẩm khô">Thực phẩm khô</a></li>
-                                <li><a href="/c-94/thuc-pham-tuoi-song.html" title="Thực phẩm tươi sống">Thực phẩm tươi sống</a></li>
-                                <li><a href="/c-95/rau-cu-qua.html" title="Rau - Củ - Quả">Rau - Củ - Quả</a></li>
                                 <li><a href="/c-192/thuc-pham-dong-lanh.html" title="Thực phẩm đông lạnh">Thực phẩm đông lạnh</a></li>
                                 <li><a href="/c-193/bia-ruou-giai-khat.html" title="Bia rượu - giải khát">Bia rượu - giải khát</a></li>
                                 <li><a href="/c-194/gia-vi-tap-hoa.html" title="Gia vị - tạp hóa">Gia vị - tạp hóa</a></li>
@@ -125,10 +107,6 @@
                                 <li><a href="/c-218/noi-that-phong-an-bep.html" title="Nội thất phòng ăn - bếp">Nội thất phòng ăn - bếp</a></li>
                                 <li><a href="/c-219/san-vuon.html" title="Sân - vườn ">Sân - vườn </a></li>
                                 <li><a href="/c-220/vat-lieu-tan-trang-nha.html" title="Vật liệu tân trang nhà">Vật liệu tân trang nhà</a></li>
-                                <li><a href="/c-221/hoa-pham-chat-giat-tay.html" title="Hóa phẩm - chất giặt tẩy">Hóa phẩm - chất giặt tẩy</a></li>
-                                <li><a href="/c-222/don-vi-thi-cong-sua-chua-nha.html" title="Đơn vị thi công - sửa chữa nhà">Đơn vị thi công - sửa chữa nhà</a></li>
-                                <li><a href="/c-223/dich-vu-lau-don-nha.html" title="Dịch vụ lau - dọn nhà">Dịch vụ lau - dọn nhà</a></li>
-                                <li><a href="/c-224/vat-lieu-xay-dung.html" title="Vật liệu xây dựng">Vật liệu xây dựng</a></li>
                             </ul>
                         </div>
                     </li>
@@ -144,8 +122,6 @@
                                 <li><a href="/c-141/may-tinh-bang.html" title="Máy tính bảng">Máy tính bảng</a></li>
                                 <li><a href="/c-143/may-in.html" title="Máy in">Máy in</a></li>
                                 <li><a href="/c-144/man-hinh.html" title="Màn hình">Màn hình</a></li>
-                                <li><a href="/c-145/may-anh-may-quay.html" title="Máy ảnh - Máy quay">Máy ảnh - Máy quay</a></li>
-                                <li><a href="/c-147/thiet-bi-an-ninh.html" title="Thiết bị an ninh">Thiết bị an ninh</a></li>
                             </ul>
                             <ul>
                                 <li><a href="/c-148/tivi-am-thanh-thiet-bi-so.html" title="Tivi - Âm thanh - Thiết bị Số">Tivi - Âm thanh - Thiết bị Số</a></li>
