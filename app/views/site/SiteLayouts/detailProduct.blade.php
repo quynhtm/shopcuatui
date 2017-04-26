486e0c73573ac3b079cb42d2029bfa8980189659
nguyenduypt86@gmail.com <nguyenduypt86@gmail.com>
2017-04-26 23:17:23 +0700

    duy

---
+++ app/views/site/SiteLayouts/detailProduct.blade.php
@@ -0,0 +1,145 @@
+<div class="container">
+    <div class="main-view-post">
+        <h1 class="title-head">
+            <a title="Thời trang nữ" href="">Thời trang nữ</a>
+        </h1>
+        <div class="row">
+            <div class="left-slider-img">
+                <div class="img-main-view">
+                    <a href="" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                        <img src="http://sanphamredep.com/uploads/thumbs/product/261/800x800/10-17-39-22-09-2016-1.jpg" alt="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                    </a>
+                </div>
+                <div class="arr-img">
+                    <div id="slick">
+                        <div class="item-one-img-view">
+                            <a href="" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" tabindex="0">
+                                <img src="http://sanphamredep.com/uploads/thumbs/product/261/400x400/10-17-39-22-09-2016-1.jpg" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" alt="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                            </a>
+                        </div>
+                        <div class="item-one-img-view">
+                            <a href="" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" tabindex="0">
+                                <img src="http://sanphamredep.com/uploads/thumbs/product/261/400x400/10-17-39-22-09-2016-1.jpg" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" alt="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                            </a>
+                        </div>
+                        <div class="item-one-img-view">
+                            <a href="" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" tabindex="0">
+                                <img src="http://sanphamredep.com/uploads/thumbs/product/261/400x400/10-17-39-22-09-2016-1.jpg" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" alt="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                            </a>
+                        </div>
+                        <div class="item-one-img-view">
+                            <a href="" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" tabindex="0">
+                                <img src="http://sanphamredep.com/uploads/thumbs/product/261/400x400/10-17-39-22-09-2016-1.jpg" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" alt="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                            </a>
+                        </div>
+                        <div class="item-one-img-view">
+                            <a href="" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" tabindex="0">
+                                <img src="http://sanphamredep.com/uploads/thumbs/product/261/400x400/10-17-39-22-09-2016-1.jpg" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" alt="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                            </a>
+                        </div>
+                        <div class="item-one-img-view">
+                            <a href="" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" tabindex="0">
+                                <img src="http://sanphamredep.com/uploads/thumbs/product/261/400x400/10-17-39-22-09-2016-1.jpg" title="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở" alt="Đồng hồ Fedylon FE-245- Phong cách chỉn chu sành điệu cho anh chàng công sở">
+                            </a>
+                        </div>
+                    </div>
+                    <script type="text/javascript">
+                        $(document).ready(function(){
+                            $("#slick").slick({
+                                dots: false,
+                                infinite: true,
+                                slidesToShow: 5,
+                                slidesToScroll: 3
+                            });
+                        });
+                    </script>
+                </div>
+            </div>
+            <div class="center-des-product">
+                <h1>Dầu óc chó Nga 100%</h1>
+                <div class="row-price">
+                    <div class="lbl-row">Giá thị trường:</div>
+                    <div class="price-origin">380.000đ</div>
+                </div>
+                <div class="row-price">
+                    <div class="lbl-row lbl-price-sale">Giá bán:</div>
+                    <div class="price-sale">330.000<span class="td-border">đ</span></div>
+                </div>
+                <div class="features-point">
+                    <div class="lbl-point">Mô tả sản phẩm</div>
+                    <div class="des-point"><p>Dầu óc chó Nga 100% ép từ hạt óc chó, đóng chai thủy tinh và 100% không chất bảo quản.&nbsp;<br>rnChai 250ml&nbsp;<br>rnThành phần dinh dưỡng: 99.8% chất béo. Trong 100 gam chứa 89.8 calo. Hàm lượng Omega 3 và 6 cao nhất trong các loại hạt hay cá hồi. Giúp phát triển trí não, phát triển trí thông minh của trẻ, cho phụ nữ mang thai,tăng cường trí nhớ cho người cao tuổi.</p></div>
+                    <div class="box-promotion">
+                        <div class="lbl-point">Thông tin khuyến mãi</div>
+                        <div class="box-content-promotion">Mua 2 chai giảm thêm 5%</div>
+                    </div>
+                </div>
+            </div>
+            <div class="right-des-product">
+                <div class="content-right-product">
+                    <div id="fb-root"></div>
+                    <script>(function(d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (d.getElementById(id)) return;
+                            js = d.createElement(s); js.id = id;
+                            js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.6";
+                            fjs.parentNode.insertBefore(js, fjs);
+                        }(document, 'script', 'facebook-jssdk'));</script>
+                    <div class="fb-like" data-href="http://shopcuatui.com.vn.ver2/tap-hoa/664-dau-oc-cho-nga-100.html" data-layout="button_count" data-action="like" data-show-faces="false" data-share="true">
+                    </div>
+                </div>
+                <div class="content-right-product">
+                    <div class="order-number">
+                        <label for="buy-number">Số lượng</label>
+                        <select class="sl-num" id="buy-num" name="buy-num">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                        </select>
+                    </div>
+                    <div id="buttonFormBuySubmit" data-pid="664" class="buynow btn">Mua ngay</div>
+                </div>
+                <div class="content-right-product">
+                    <div class="order-number-phone">
+                        <p><b>Đặt nhanh qua điện thoại</b></p>
+                        <div class="number-phone">
+                            <div class="fa fa-phone"></div>
+                            <span>0913922986</span>
+                        </div>
+                        <p><a href="" title="Shop: Siêu thị gia đình">Siêu thị gia đình</a></p>
+                        <p><b>Thông tin liên hệ: </b></p>
+                        <p>nguyenduypt86@gmail.com</p>
+                        <p>Việt Hưng - Long Biên - Hà Nội</p>
+                    </div>
+                    <div class="link-fast">
+                        <p><a href="#gioi-thieu-shop">Giới thiệu Shop</a></p>
+                        <p><a href="#chinh-sach-van-chuyen">Chính sách vận chuyển</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="line-box line-box-cat vip">
+            <div class="cate-box">
+                <div class="inner-cate-box hide-text-over">
+                    <h2 class="parent-cate act">
+                        <a href="javascript:void(0)">Chi tiết sản phẩm</a>
+                    </h2>
+                </div>
+            </div>
+            <div class="content-bottom-content-view">
+                Dầu óc chó Nga 100% ép từ hạt óc chó, đóng chai thủy tinh và 100% không chất bảo quản.
+                rnChai 250ml
+                rnHạn sử dụng 1 năm kể từ ngày sản xuất in trên bao bì, sau khi bóc bảo quản trong ngăn mát tủ lạnh không bảo quản bên ngoài bởi dầu óc chó 100% không có chất bảo quản không giống như một số hãng. Tuyệt đối không nấu, chiên xào, làm nóng dầu óc chó.
+                rn(Có thể có lắng cặn tự nhiên trong quá trình sử dụng)
+                rnThành phần dinh dưỡng: 99.8% chất béo. Trong 100 gam chứa 89.8 calo. Hàm lượng Omega 3 và 6 cao nhất trong các loại hạt hay cá hồi. Giúp phát triển trí não, phát triển trí thông minh của trẻ, cho phụ nữ mang thai,tăng cường trí nhớ cho người cao tuổi.
+            </div>
+        </div>
+    </div>
+</div>
+
---
+++ app/views/site/SiteLayouts/listProduct.blade.php
@@ -0,0 +1,136 @@
+<div class="container">
+    <div class="main-view-post">
+        <h1 class="title-head">
+            <a title="Thời trang nữ" href="">Thời trang nữ</a>
+        </h1>
+        <div class="list-link-cat">
+            <ul>
+                <li><a href="ao-so-mi-nu-483.html" title="Áo sơ mi nữ">Áo sơ mi nữ</a></li>
+                <li><a href="ao-khoac-nu-484.html" title="Áo khoác nữ">Áo khoác nữ</a></li>
+                <li><a href="quan-au-nu-485.html" title="Quần âu nữ">Quần âu nữ</a></li>
+                <li><a href="quan-jean-nu-486.html" title="Quần jean nữ">Quần jean nữ</a></li>
+                <li><a href="giay-dep-nu-487.html" title="Giày dép nữ">Giày dép nữ</a></li>
+                <li><a href="balo-tui-xach-nu-488.html" title="Balo, túi xách nữ">Balo, túi xách nữ</a></li>
+                <li><a href="dam-vay-nu-489.html" title="Đầm váy nữ">Đầm váy nữ</a></li>
+                <li><a href="do-ngu-nu-490.html" title="Đồ ngủ nữ">Đồ ngủ nữ</a></li>
+                <li><a href="ao-phong-nu-508.html" title="Áo phông nữ">Áo phông nữ</a></li>
+                <li><a href="quan-ao-the-thao-nu-510.html" title="Quần áo thể thao nữ">Quần áo thể thao nữ</a></li>
+                <li><a href="dong-ho-nu-519.html" title="Đồng hồ nữ">Đồng hồ nữ</a></li>
+            </ul>
+            <div class="click-more-view-cat"><i class="fa fa-angle-down"></i></div>
+        </div>
+        <div class="content-list-item ">
+            <ul class="data-tab tab-0 act">
+                <li class="item">
+                    <span class="sale-off">
+                        -10%
+                    </span>
+
+
+                    <div class="post-thumb">
+                        <a title="" href="">
+                            <img src="https://static11.muachungcdn.com/thumb,80/240_240/i:plaza/product/product/-0-unnamed%20(3)-147997308770751/nuoc-rua-tay-dang-bot-huong-cam-200ml.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="item-content">
+                        <div class="title-info">
+                            <h4 class="post-title">
+                                <a title="" href="">Sản phẩm demo</a>
+                            </h4>
+                            <div class="item-price">
+                                <span class="amount-1">200.000đ</span>
+                                <span class="amount-2">300.000đđ</span>
+                            </div>
+                        </div>
+                        <div class="mgt5 amount-call">
+                            <a title="" class="link-shop" href="">Shopcuatui</a>
+                        </div>
+
+                    </div>
+                </li>
+                <li class="item">
+                    <span class="sale-off">
+                        -10%
+                    </span>
+
+
+                    <div class="post-thumb">
+                        <a title="" href="">
+                            <img src="https://static11.muachungcdn.com/thumb,80/240_240/i:plaza/product/product/-0-unnamed%20(3)-147997308770751/nuoc-rua-tay-dang-bot-huong-cam-200ml.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="item-content">
+                        <div class="title-info">
+                            <h4 class="post-title">
+                                <a title="" href="">Sản phẩm demo</a>
+                            </h4>
+                            <div class="item-price">
+                                <span class="amount-1">200.000đ</span>
+                                <span class="amount-2">300.000đđ</span>
+                            </div>
+                        </div>
+                        <div class="mgt5 amount-call">
+                            <a title="" class="link-shop" href="">Shopcuatui</a>
+                        </div>
+
+                    </div>
+                </li>
+                <li class="item">
+                    <span class="sale-off">
+                        -10%
+                    </span>
+
+
+                    <div class="post-thumb">
+                        <a title="" href="">
+                            <img src="https://static11.muachungcdn.com/thumb,80/240_240/i:plaza/product/product/-0-unnamed%20(3)-147997308770751/nuoc-rua-tay-dang-bot-huong-cam-200ml.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="item-content">
+                        <div class="title-info">
+                            <h4 class="post-title">
+                                <a title="" href="">Sản phẩm demo</a>
+                            </h4>
+                            <div class="item-price">
+                                <span class="amount-1">200.000đ</span>
+                                <span class="amount-2">300.000đđ</span>
+                            </div>
+                        </div>
+                        <div class="mgt5 amount-call">
+                            <a title="" class="link-shop" href="">Shopcuatui</a>
+                        </div>
+
+                    </div>
+                </li>
+                <li class="item">
+                    <span class="sale-off">
+                        -10%
+                    </span>
+
+
+                    <div class="post-thumb">
+                        <a title="" href="">
+                            <img src="https://static11.muachungcdn.com/thumb,80/240_240/i:plaza/product/product/-0-unnamed%20(3)-147997308770751/nuoc-rua-tay-dang-bot-huong-cam-200ml.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="item-content">
+                        <div class="title-info">
+                            <h4 class="post-title">
+                                <a title="" href="">Sản phẩm demo</a>
+                            </h4>
+                            <div class="item-price">
+                                <span class="amount-1">200.000đ</span>
+                                <span class="amount-2">300.000đđ</span>
+                            </div>
+                        </div>
+                        <div class="mgt5 amount-call">
+                            <a title="" class="link-shop" href="">Shopcuatui</a>
+                        </div>
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
